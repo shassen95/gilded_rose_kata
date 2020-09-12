31d1842445cc1aa3551da0ff63b718fe4331ec8a
Sami Ibrahim <ibrahims@magellanhealth.com>
2020-09-12 15:55:15 -0400

    Records that sell_in can change to 0 from 1

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 
 describe("Gilded Rose", function() {
 
-    it("should decrease sell_in by 1", function() {   
+    it("should decrease sell_in from 10 to 9", function() {   
         const items = [];
 
         items.push(new gilded_rose.Item('+5 Dexterity Vest', 10, 20));     
@@ -13,7 +13,7 @@ describe("Gilded Rose", function() {
         assert.equal(item.sell_in, 9);
     });
 
-    it("should decrease quality by 1", function() {   
+    it("should decrease quality from 20 to 19", function() {   
         const items = [];
 
         items.push(new gilded_rose.Item('+5 Dexterity Vest', 10, 20));     
@@ -23,12 +23,12 @@ describe("Gilded Rose", function() {
         assert.equal(item.quality, 19);
     });
 
-    it("x", function() {   
+    it("should decrease sell_in from 1 to 0", function() {   
         const items = [];
 
         items.push(new gilded_rose.Item('+5 Dexterity Vest', 1, 20));     
         const updated_items = gilded_rose.update_quality(items);
-        assert.equal(updated_items, null);
+        assert.equal(updated_items[0].sell_in, 0);
     });
 
 
