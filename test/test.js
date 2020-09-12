2aec6b8aee9b139b6d4352f846d7456e6be79e8f
Sami Ibrahim <ibrahims@magellanhealth.com>
2020-09-12 15:48:52 -0400

    Records that the quality value for +5 Dexterity Vest' drops by 1

--- test/test.js
+++ test/test.js
@@ -13,14 +13,14 @@ describe("Gilded Rose", function() {
         assert.equal(item.sell_in, 9);
     });
 
-    it("x", function() {   
+    it("should decrease quality by 1", function() {   
         var items = []
 
         items.push(new gilded_rose.Item('+5 Dexterity Vest', 10, 20));     
         const updated_items = gilded_rose.update_quality(items);
         const item = updated_items[0];
 
-        assert.equal(item.quality, null);
+        assert.equal(item.quality, 19);
     });
   
   });
