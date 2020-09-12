e6d989ac34c69c86e1acc11464d7f3498c8d128a
Sami Ibrahim <ibrahims@magellanhealth.com>
2020-09-12 16:12:20 -0400

    Creates a test where sell_in is 10 and quality is -1 for a standard item

--- test/test.js
+++ test/test.js
@@ -71,6 +71,14 @@ describe("Gilded Rose", function() {
         assert.equal(updated_items[0].quality, 0);
     });
 
+    it("x", function() {   
+        const items = [];
+
+        items.push(new gilded_rose.Item('+5 Dexterity Vest', 10, -1));     
+        const updated_items = gilded_rose.update_quality(items);
+        assert.equal(updated_items, null);
+    });
+
   
   });
   
