99b9825574f4d15ccd0f513b5823d6ea5897666a
Sami Ibrahim <ibrahims@magellanhealth.com>
2020-09-12 17:03:46 -0400

    Records that quality should not change when its -30 and sell_in is 50

--- test/test.js
+++ test/test.js
@@ -139,12 +139,12 @@ describe("Gilded Rose", function() {
             assert.equal(updated_items[0].quality, 0);
         });
 
-        it("x", function() {   
+        it("should not change quality when its -30 and sell_in is 50", function() {   
             const items = [];
 
             items.push(new gilded_rose.Item('Sulfuras, Hand of Ragnaros', 50, -30)); 
             const updated_items = gilded_rose.update_quality(items);
-            assert.equal(updated_items, null);
+            assert.equal(updated_items[0].quality, -30);
         });
 
     })
