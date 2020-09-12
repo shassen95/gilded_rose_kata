9926ea2f53b99e4c5e0123bda83813b63e5aa6b9
Sami Ibrahim <ibrahims@magellanhealth.com>
2020-09-12 16:46:08 -0400

    Creates a test for a legendary item where sell_in is 50 and quality is 80

--- test/test.js
+++ test/test.js
@@ -107,10 +107,18 @@ describe("Gilded Rose", function() {
             assert.equal(updated_items[0].sell_in, -1);
         });
 
-        it("x", function() {   
+        it("should not change sell_in when its -50", function() {   
             const items = [];
 
             items.push(new gilded_rose.Item('Sulfuras, Hand of Ragnaros', -50, 80)); 
+            const updated_items = gilded_rose.update_quality(items);
+            assert.equal(updated_items[0].sell_in, -50);
+        });
+
+        it("x", function() {   
+            const items = [];
+
+            items.push(new gilded_rose.Item('Sulfuras, Hand of Ragnaros', 50, 80)); 
             const updated_items = gilded_rose.update_quality(items);
             assert.equal(updated_items, null);
         });
