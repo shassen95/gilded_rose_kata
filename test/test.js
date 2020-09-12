54dbbf7182f8b318f80b7567b111b9618cd75f85
Sami Ibrahim <ibrahims@magellanhealth.com>
2020-09-12 15:35:25 -0400

    Creates first characterization test

--- test/test.js
+++ test/test.js
@@ -1,9 +1,14 @@
 const gilded_rose = require('../src/gilded_rose');
+const assert = require('assert');
 
 describe("Gilded Rose", function() {
 
-    it("x", function() {        
-      console.log("test "+gilded_rose.Item);
+    it("x", function() {   
+        var items = []
+
+        items.push(new Item('+5 Dexterity Vest', 10, 20));     
+        const updated_items = gilded_rose.update_quality(items);
+        assert.equal(null, updated_items);
     });
   
   });
