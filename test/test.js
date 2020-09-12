a817474c126e01b876b350d4177d505ff4c37503
Sami Ibrahim <ibrahims@magellanhealth.com>
2020-09-12 16:07:27 -0400

    Records that quality drops from 1 to 0 for a standard item

--- test/test.js
+++ test/test.js
@@ -55,12 +55,12 @@ describe("Gilded Rose", function() {
         assert.equal(updated_items[0].sell_in, -2);
     });
 
-    it("x", function() {   
+    it("should decrease quality to 0", function() {   
         const items = [];
 
         items.push(new gilded_rose.Item('+5 Dexterity Vest', 10, 1));     
         const updated_items = gilded_rose.update_quality(items);
-        assert.equal(updated_items, null);
+        assert.equal(updated_items[0].quality, 0);
     });
 
   
