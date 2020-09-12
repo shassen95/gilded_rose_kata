76136dda9ca618fd10f96863e8eb28b8fd20a24e
Sami Ibrahim <ibrahims@magellanhealth.com>
2020-09-12 16:03:50 -0400

    Creates that when sell_in is -1 and quality is 20 for a standard item

--- test/test.js
+++ test/test.js
@@ -39,13 +39,20 @@ describe("Gilded Rose", function() {
         assert.equal(updated_items[0].sell_in, -1);
     });
 
-
     it("should decrease quality from 20 to 18", function() {   
         const items = [];
 
         items.push(new gilded_rose.Item('+5 Dexterity Vest', 0, 20));     
         const updated_items = gilded_rose.update_quality(items);
         assert.equal(updated_items[0].quality, 18);
+    });
+
+    it("x", function() {   
+        const items = [];
+
+        items.push(new gilded_rose.Item('+5 Dexterity Vest', -1, 20));     
+        const updated_items = gilded_rose.update_quality(items);
+        assert.equal(updated_items, null);
     });
 
   
