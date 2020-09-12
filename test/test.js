1e47aa1591edeef3361e319388a47dc59219336b
Sami Ibrahim <ibrahims@magellanhealth.com>
2020-09-12 15:42:04 -0400

    Records that the sell_in value for '+5 Dexterity Vest' drops by 1

--- test/test.js
+++ test/test.js
@@ -3,12 +3,14 @@ const assert = require('assert');
 
 describe("Gilded Rose", function() {
 
-    it("x", function() {   
+    it("should decrease sell_in by 1", function() {   
         var items = []
 
-        items.push(new Item('+5 Dexterity Vest', 10, 20));     
+        items.push(new gilded_rose.Item('+5 Dexterity Vest', 10, 20));     
         const updated_items = gilded_rose.update_quality(items);
-        assert.equal(null, updated_items);
+        const item = updated_items[0];
+
+        assert.equal(item.sell_in, 9);
     });
   
   });
