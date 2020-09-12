15563ed43bf51524205df07a392203eb84dd3057
Sami Ibrahim <ibrahims@magellanhealth.com>
2020-09-12 15:58:32 -0400

    Records that sell_in can change to -1 from 0 for a standard item

--- test/test.js
+++ test/test.js
@@ -31,12 +31,12 @@ describe("Gilded Rose", function() {
         assert.equal(updated_items[0].sell_in, 0);
     });
 
-    it("x", function() {   
+    it("should decrease sell_in from 0 to -1", function() {   
         const items = [];
 
         items.push(new gilded_rose.Item('+5 Dexterity Vest', 0, 20));     
         const updated_items = gilded_rose.update_quality(items);
-        assert.equal(updated_items, null);
+        assert.equal(updated_items[0].sell_in, -1);
     });
 
 
