b6c675a0aadd3712a5e77eb57b32daecb8ec71e5
Sami Ibrahim <ibrahims@magellanhealth.com>
2020-09-12 19:28:46 -0400

    Creates a test for backstage where sell_in is 0 and quality is 20

--- test/test.js
+++ test/test.js
@@ -172,6 +172,16 @@ describe("Gilded Rose", function() {
                 assert.equal(updated_items[0].sell_in, sell_in-1);
             });
 
+            it("x", function() {   
+                const items = [];
+    
+                const sell_in = 0;
+                const quality = 20;
+                items.push(new gilded_rose.Item('Backstage passes to a TAFKAL80ETC concert', sell_in, quality));
+                const updated_items = gilded_rose.update_quality(items);
+                assert.equal(updated_items, null);
+            });
+
         });
     });
 });
