a0838df9adc12d1434ee94ed8fd6e86949918aac
Sami Ibrahim <ibrahims@magellanhealth.com>
2020-09-12 20:11:32 -0400

    Organizes test cases

--- test/test.js
+++ test/test.js
@@ -3,148 +3,158 @@ const assert = require('assert');
 
 describe("Gilded Rose", function() {
     describe("Standard Items", function(){
+        describe("Sell-In", function(){
+            it("should decrease from 10 to 9 when quality is 20", function() {   
+                const items = [];
+    
+                items.push(new gilded_rose.Item('+5 Dexterity Vest', 10, 20));     
+                const updated_items = gilded_rose.update_quality(items);
+                const item = updated_items[0];
+    
+                assert.equal(item.sell_in, 9);
+            });
 
-        it("should decrease sell_in from 10 to 9", function() {   
-            const items = [];
+            it("should decrease from 1 to 0 when quality is 20", function() {   
+                const items = [];
+    
+                items.push(new gilded_rose.Item('+5 Dexterity Vest', 1, 20));     
+                const updated_items = gilded_rose.update_quality(items);
+                assert.equal(updated_items[0].sell_in, 0);
+            });
 
-            items.push(new gilded_rose.Item('+5 Dexterity Vest', 10, 20));     
-            const updated_items = gilded_rose.update_quality(items);
-            const item = updated_items[0];
+            it("should decrease from 0 to -1 when quality is 20", function() {   
+                const items = [];
+    
+                items.push(new gilded_rose.Item('+5 Dexterity Vest', 0, 20));     
+                const updated_items = gilded_rose.update_quality(items);
+                assert.equal(updated_items[0].sell_in, -1);
+            });
 
-            assert.equal(item.sell_in, 9);
+            it("should decrease from -1 to -2 when quality is 20", function() {   
+                const items = [];
+    
+                items.push(new gilded_rose.Item('+5 Dexterity Vest', -1, 20));     
+                const updated_items = gilded_rose.update_quality(items);
+                assert.equal(updated_items[0].sell_in, -2);
+            });
+
         });
 
-        it("should decrease quality from 20 to 19", function() {   
-            const items = [];
-
-            items.push(new gilded_rose.Item('+5 Dexterity Vest', 10, 20));     
-            const updated_items = gilded_rose.update_quality(items);
-            const item = updated_items[0];
-
-            assert.equal(item.quality, 19);
-        });
-
-        it("should decrease sell_in from 1 to 0", function() {   
-            const items = [];
-
-            items.push(new gilded_rose.Item('+5 Dexterity Vest', 1, 20));     
-            const updated_items = gilded_rose.update_quality(items);
-            assert.equal(updated_items[0].sell_in, 0);
-        });
-
-        it("should decrease sell_in from 0 to -1", function() {   
-            const items = [];
-
-            items.push(new gilded_rose.Item('+5 Dexterity Vest', 0, 20));     
-            const updated_items = gilded_rose.update_quality(items);
-            assert.equal(updated_items[0].sell_in, -1);
-        });
-
-        it("should decrease quality from 20 to 18", function() {   
-            const items = [];
-
-            items.push(new gilded_rose.Item('+5 Dexterity Vest', 0, 20));     
-            const updated_items = gilded_rose.update_quality(items);
-            assert.equal(updated_items[0].quality, 18);
-        });
-
-        it("should decrease sell_in from -1 to -2", function() {   
-            const items = [];
-
-            items.push(new gilded_rose.Item('+5 Dexterity Vest', -1, 20));     
-            const updated_items = gilded_rose.update_quality(items);
-            assert.equal(updated_items[0].sell_in, -2);
-        });
-
-        it("should decrease quality to 0", function() {   
-            const items = [];
-
-            items.push(new gilded_rose.Item('+5 Dexterity Vest', 10, 1));     
-            const updated_items = gilded_rose.update_quality(items);
-            assert.equal(updated_items[0].quality, 0);
-        });
-
-        it("should not change the quality when it is 0", function() {   
-            const items = [];
-
-            items.push(new gilded_rose.Item('+5 Dexterity Vest', 10, 0));     
-            const updated_items = gilded_rose.update_quality(items);
-            assert.equal(updated_items[0].quality, 0);
-        });
-
-        it("should not change the quality when its -1", function() {   
-            const items = [];
-
-            items.push(new gilded_rose.Item('+5 Dexterity Vest', 10, -1));     
-            const updated_items = gilded_rose.update_quality(items);
-            assert.equal(updated_items[0].quality, -1);
-        });
-
-        it("should decrease the quality from 51 to 50", function() {   
-            const items = [];
-
-            items.push(new gilded_rose.Item('+5 Dexterity Vest', 10, 51));     
-            const updated_items = gilded_rose.update_quality(items);
-            assert.equal(updated_items[0].quality, 50);
+        describe("Quality", function(){
+            it("should decrease from 20 to 19 when sell_in is 10", function() {   
+                const items = [];
+    
+                items.push(new gilded_rose.Item('+5 Dexterity Vest', 10, 20));     
+                const updated_items = gilded_rose.update_quality(items);
+                const item = updated_items[0];
+    
+                assert.equal(item.quality, 19);
+            });
+    
+            it("should decrease from 20 to 18 when sell_in is 0", function() {   
+                const items = [];
+    
+                items.push(new gilded_rose.Item('+5 Dexterity Vest', 0, 20));     
+                const updated_items = gilded_rose.update_quality(items);
+                assert.equal(updated_items[0].quality, 18);
+            });
+    
+            it("should decrease from 1 to 0 when sell_in is 10", function() {   
+                const items = [];
+    
+                items.push(new gilded_rose.Item('+5 Dexterity Vest', 10, 1));     
+                const updated_items = gilded_rose.update_quality(items);
+                assert.equal(updated_items[0].quality, 0);
+            });
+    
+            it("should not change when it is 0 and when sell_in is 10", function() {   
+                const items = [];
+    
+                items.push(new gilded_rose.Item('+5 Dexterity Vest', 10, 0));     
+                const updated_items = gilded_rose.update_quality(items);
+                assert.equal(updated_items[0].quality, 0);
+            });
+    
+            it("should not change when its -1 and when sell_in is 10", function() {   
+                const items = [];
+    
+                items.push(new gilded_rose.Item('+5 Dexterity Vest', 10, -1));     
+                const updated_items = gilded_rose.update_quality(items);
+                assert.equal(updated_items[0].quality, -1);
+            });
+    
+            it("should decrease from 51 to 50 when sell_in is 10", function() {   
+                const items = [];
+    
+                items.push(new gilded_rose.Item('+5 Dexterity Vest', 10, 51));     
+                const updated_items = gilded_rose.update_quality(items);
+                assert.equal(updated_items[0].quality, 50);
+            });
         });
     });
 
     describe("Legendary Items", function(){
 
-        it("should not change sell_in when it is 0", function() {   
-            const items = [];
+        describe("Sell-In", function(){
 
-            items.push(new gilded_rose.Item('Sulfuras, Hand of Ragnaros', 0, 80)); 
-            const updated_items = gilded_rose.update_quality(items);
-            assert.equal(updated_items[0].sell_in, 0);
+            it("should not change when it is 0 and quality is 80", function() {   
+                const items = [];
+
+                items.push(new gilded_rose.Item('Sulfuras, Hand of Ragnaros', 0, 80)); 
+                const updated_items = gilded_rose.update_quality(items);
+                assert.equal(updated_items[0].sell_in, 0);
+            });
+
+            it("should not change when its -1 and quality is 80", function() {   
+                const items = [];
+
+                items.push(new gilded_rose.Item('Sulfuras, Hand of Ragnaros', -1, 80)); 
+                const updated_items = gilded_rose.update_quality(items);
+                assert.equal(updated_items[0].sell_in, -1);
+            });
+
+            it("should not change when its -50 and quality is 80", function() {   
+                const items = [];
+
+                items.push(new gilded_rose.Item('Sulfuras, Hand of Ragnaros', -50, 80)); 
+                const updated_items = gilded_rose.update_quality(items);
+                assert.equal(updated_items[0].sell_in, -50);
+            });
+
+            it("should not change when its 50 and quality is 80", function() {   
+                const items = [];
+
+                items.push(new gilded_rose.Item('Sulfuras, Hand of Ragnaros', 50, 80)); 
+                const updated_items = gilded_rose.update_quality(items);
+                assert.equal(updated_items[0].sell_in, 50);
+            }); 
         });
 
-        it("should not change sell_in when its -1", function() {   
-            const items = [];
+        describe("Quality", function(){
 
-            items.push(new gilded_rose.Item('Sulfuras, Hand of Ragnaros', -1, 80)); 
-            const updated_items = gilded_rose.update_quality(items);
-            assert.equal(updated_items[0].sell_in, -1);
-        });
+            it("should not change when its 80 and sell_in is 50", function() {   
+                const items = [];
 
-        it("should not change sell_in when its -50", function() {   
-            const items = [];
+                items.push(new gilded_rose.Item('Sulfuras, Hand of Ragnaros', 50, 80)); 
+                const updated_items = gilded_rose.update_quality(items);
+                assert.equal(updated_items[0].quality, 80);
+            });
 
-            items.push(new gilded_rose.Item('Sulfuras, Hand of Ragnaros', -50, 80)); 
-            const updated_items = gilded_rose.update_quality(items);
-            assert.equal(updated_items[0].sell_in, -50);
-        });
+            it("should not change when its 0 and sell_in is 50", function() {   
+                const items = [];
 
-        it("should not change sell_in when its 50", function() {   
-            const items = [];
+                items.push(new gilded_rose.Item('Sulfuras, Hand of Ragnaros', 50, 0)); 
+                const updated_items = gilded_rose.update_quality(items);
+                assert.equal(updated_items[0].quality, 0);
+            });
 
-            items.push(new gilded_rose.Item('Sulfuras, Hand of Ragnaros', 50, 80)); 
-            const updated_items = gilded_rose.update_quality(items);
-            assert.equal(updated_items[0].sell_in, 50);
-        });
+            it("should not change when its -30 and sell_in is 50", function() {   
+                const items = [];
 
-        it("should not change quality when its 80", function() {   
-            const items = [];
-
-            items.push(new gilded_rose.Item('Sulfuras, Hand of Ragnaros', 50, 80)); 
-            const updated_items = gilded_rose.update_quality(items);
-            assert.equal(updated_items[0].quality, 80);
-        });
-
-        it("should not change quality when its 0 and sell_in is 50", function() {   
-            const items = [];
-
-            items.push(new gilded_rose.Item('Sulfuras, Hand of Ragnaros', 50, 0)); 
-            const updated_items = gilded_rose.update_quality(items);
-            assert.equal(updated_items[0].quality, 0);
-        });
-
-        it("should not change quality when its -30 and sell_in is 50", function() {   
-            const items = [];
-
-            items.push(new gilded_rose.Item('Sulfuras, Hand of Ragnaros', 50, -30)); 
-            const updated_items = gilded_rose.update_quality(items);
-            assert.equal(updated_items[0].quality, -30);
+                items.push(new gilded_rose.Item('Sulfuras, Hand of Ragnaros', 50, -30)); 
+                const updated_items = gilded_rose.update_quality(items);
+                assert.equal(updated_items[0].quality, -30);
+            });
         });
 
     });
@@ -249,5 +259,11 @@ describe("Gilded Rose", function() {
 
         });
     });
+
+    describe("Aged Brie", function(){
+        describe("Sell-In", function() {
+            
+        })
+    })
 });
   
