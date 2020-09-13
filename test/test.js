df080887831f312f28801e55b4b134f6dfbac66c
Sami Ibrahim <ibrahims@magellanhealth.com>
2020-09-13 19:32:17 -0400

    Creates a quality test for aged brie when its 21 and sell_in is 0

--- test/test.js
+++ test/test.js
@@ -316,6 +316,16 @@ describe("Gilded Rose", function() {
                 assert.equal(updated_items[0].quality, quality+1);
             });
 
+            it("x", function() {   
+                const items = [];
+    
+                const sell_in = 0;
+                const quality = 20;
+                items.push(new gilded_rose_items.Item('Aged Brie', sell_in, quality));
+                const updated_items = gilded_rose.update_quality(items);
+                assert.equal(updated_items, null);
+            });
+
             it("should increase from 25 to 27 when sell_in is -1", function() {   
                 const items = [];
     
