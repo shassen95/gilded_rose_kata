561911cc93345366dd201d3f5005e7ea32f89a37
Sami Ibrahim <ibrahims@magellanhealth.com>
2020-09-13 13:51:24 -0400

    Forces test 1 to fail

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -12,8 +12,10 @@ exports.Item = function Item(name, sell_in, quality) {
 // items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
 // items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
 // items.push(new Item('Conjured Mana Cake', 3, 6));
+exports.update_quality = function(items){
 
-exports.update_quality = function update_quality(items) {
+}
+exports.update_quality_old = function update_quality(items) {
   for (var i = 0; i < items.length; i++) {
     if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
       if (items[i].quality > 0) {
