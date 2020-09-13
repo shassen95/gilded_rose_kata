4a5bb7d0817c4837d801f76af6384f634d8978eb
Sami Ibrahim <ibrahims@magellanhealth.com>
2020-09-12 20:35:03 -0400

    Creates a quality test where its 50 and sell_in is 1

--- test/test.js
+++ test/test.js
@@ -304,6 +304,16 @@ describe("Gilded Rose", function() {
                 const updated_items = gilded_rose.update_quality(items);
                 assert.equal(updated_items[0].quality, quality+1);
             });
+
+            it("", function() {   
+                const items = [];
+    
+                const sell_in = 1;
+                const quality = 50;
+                items.push(new gilded_rose.Item('Aged Brie', sell_in, quality));
+                const updated_items = gilded_rose.update_quality(items);
+                assert.equal(updated_items, null);
+            });
         })
     })
 });
